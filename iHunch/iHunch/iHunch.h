c3da36b59ea08d463b7d9bd2c08acf35270c79b8
JINU <201703ska@soongsil.ac.kr>
2020-09-07 21:09:04 +0900

    자세 설정 창 추가

--- iHunch/iHunch/iHunch.h
+++ iHunch/iHunch/iHunch.h
@@ -8,6 +8,7 @@
 #include <QWidget>
 #include <QMenu>
 #include <QStyle>
+#include "setupPose.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class iHunchClass; }
@@ -29,9 +30,12 @@ private:
     Ui::iHunchClass* ui;
     //트레이아이콘 클래스 인스턴스
     QSystemTrayIcon* m_trayicon;
+    setupPose* setuppose;
 
 public slots:
     //시스템 트레이 아이콘 클릭 이벤트로부터 시그널을 처리하는 slot함수
     void iconActivated(QSystemTrayIcon::ActivationReason reason);
     void popupslot();
+    //자세 설정 창 띄우기
+    void setPose();
 };
---
+++ iHunch/iHunch/setupPose.cpp
@@ -0,0 +1,11 @@
+#include "setupPose.h"
+
+setupPose::setupPose(QWidget *parent)
+	: QDialog(parent), ui(new Ui::setupPose)
+{
+	ui->setupUi(this);
+}
+
+setupPose::~setupPose()
+{
+}
